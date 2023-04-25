31dffc84165073562e036976eac1f1d0a0a59fab
nedwards-pvcc <125890065+nedwards-pvcc@users.noreply.github.com>
2023-04-24 20:46:03 -0400

    Add files via upload

---
+++ pvcc_courses_snippet.py
@@ -0,0 +1,28 @@
+# NAME: your-name-here
+# Program Purpose: This program uses PYTHON SETS to display PVCC
+#   REQUIRED courses & TECHNICAL ELECTIVE courses for:
+#   --CSC certificate in Computer & Network Support Technologies:
+#       ---required courses
+#       ---plus 3 technical electives
+#   --AAS degree in Information Technology:
+#       ---required courses
+#       ---plus 4 technical electives
+
+# create sets for  CERTIIFICATE, Computer & Network Support Technologies
+network_req = {'CSC 110', 'ETR 164', 'ITN 101',}
+  
+network_elect = {'CSC 201', 'CSC 202', 'CSC 205', 
+        'ETR 113', 'ETR 149', 'ETR 203', 'ETR 290', 
+        'ITN 106', 'ITN 120', 'ITN 151', 'ITN 170', 'ITN 260', 'ITN 290', 
+        'ITP 120', 'ITP 132', 'ITP 200', 'ITP 220', 'ITP 290', 
+        'MTH 131', 'MTH 161', 'MTH 162', 'MTH 263', }
+
+#create sets for ASSOCIATE degree, Information Technology degree:
+info_req = { 'CSC 110',  'ENG 111',  'ENG 112',  'ETR 149',  'ETR 164',  'ITD 110',
+                 'ITD 132',  'ITE 182',  'ITE 215',  'ITN 101',  'ITN 106',  'ITN 111',
+                 'ITP 120',  'MTH 131',}
+
+info_elect ={'ITN 170', 'ITN 208', 'ITN 260', 'ITN 261',
+                       'ITN 276', 'ITN 277', 'ITP 132', 'ITP 136', 'ITP 150', 'ITP 220',}
+
+dash_line = "------------------------------------------------------"
